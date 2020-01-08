7d2a63c2c128fd1df759171cd10187c24b9bde4e
tawny francis <tawnyfrancis@gmail.com>
2020-01-08 13:45:21 -0500

    Updates

---
+++ precache-manifest.44a5708bb4217e99959a1b210afc6100.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386cfa4ed6ea15a872e35a976f4b1961",
+    "url": "/newclicky/index.html"
+  },
+  {
+    "revision": "7f19c7b30ef9cde84caf",
+    "url": "/newclicky/static/css/main.612d7846.chunk.css"
+  },
+  {
+    "revision": "a05cf66363b8ca4add6f",
+    "url": "/newclicky/static/js/2.a76d27e1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/newclicky/static/js/2.a76d27e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f19c7b30ef9cde84caf",
+    "url": "/newclicky/static/js/main.a2a611dc.chunk.js"
+  },
+  {
+    "revision": "760b060774d01ec9157b",
+    "url": "/newclicky/static/js/runtime-main.37a0b1d5.js"
+  }
+]);
